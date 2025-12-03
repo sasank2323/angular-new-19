37a6be8fc2935a9095d2d7021dcf94b3cfea1649
Tottadi sai sasank <saisasank2323@gmail.com>
2025-12-03 07:51:46 +0530

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Login } from './login/login';
+import { Signup } from './signup/signup';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Login,Signup],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('first_project');
+  title2="sasank";
 }
