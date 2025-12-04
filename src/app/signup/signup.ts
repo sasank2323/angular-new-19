b7e11128dc5ba4a239b516aca89243c825b6b064
Tottadi sai sasank <saisasank2323@gmail.com>
2025-12-04 07:38:30 +0530

    for for objects and array

--- src/app/signup/signup.ts
+++ src/app/signup/signup.ts
@@ -12,6 +12,75 @@ import { Component } from '@angular/core';
 export class Signup {
 
   x=20;
+  numbers = [10, 20, 30, 40];
+
+  users = [
+  { id: 1, name: "Sasank" },
+  { id: 2, name: "Ravi" },
+  { id: 3, name: "Kiran" }
+];
+
+userDetails = {
+  sasank: { age: 23, city: "Hyderabad" },
+  ravi: { age: 25, city: "Bangalore" },
+};
+
+employee = {
+  name: "Sasank",
+  skills: ["PHP", "Angular", "SQL"]
+};
+
+products = [
+  { name: "Laptop", price: 45000 },
+  { name: "Phone", price: 15000 }
+];
+
+
+matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+];
+
+
+department = {
+  hr: ["Anu", "Ravi"],
+  tech: ["Sasank", "Kiran", "Vamsi"]
+};
+
+companies = [
+  {
+    name: "Google",
+    jobs: ["Engineer", "Manager"]
+  },
+  {
+    name: "Microsoft",
+    jobs: ["Dev", "Tester"]
+  }
+];
+
+
+fullData = [
+  {
+    id: 1,
+    name: "Sasank",
+    address: {
+      city: "Hyderabad",
+      codes: [500081, 500072]
+    }
+  },
+  {
+    id: 2,
+    name: "Ravi",
+    address: {
+      city: "Chennai",
+      codes: [600001, 600045]
+    }
+  }
+];
+
+
+
   hide()
   {
     this.x=0;
