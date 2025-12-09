e9ebb0b40faaaf6ff65f2752963f979cb1913eb0
Tottadi sai sasank <saisasank2323@gmail.com>
2025-12-09 22:13:12 +0530

    signals

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,13 +1,31 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal,WritableSignal } from '@angular/core';
 import { Signup } from '../signup/signup';
+import { NgFor } from '@angular/common';
+
 
 @Component({
   selector: 'app-login',
+  standalone: true,
   imports: [Signup],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
   count=signal(10);
+  colors = signal(['red', 'green', 'blue']);
   x=10;
+ss: WritableSignal<number> = signal(5);
+
+  constructor() {   
+  this.count.set(this.count() + 1);
+  }
+
+  increment(){
+   // this.count.set(this.count() + 1);
+   this.count.update(n => n + 1);
+   this.colors.update(cols => [...cols, 'yellow']);
+
+  }
+
+
 }
