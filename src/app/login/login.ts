e2dfffd1429b0f7c4def6e58c7f6a65655e598cc
Tottadi sai sasank <saisasank2323@gmail.com>
2025-12-09 22:54:17 +0530

    constructor run once effect runs every signal chaNGE

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,4 +1,4 @@
-import { Component, signal,WritableSignal,computed } from '@angular/core';
+import { Component, signal,WritableSignal,computed,effect } from '@angular/core';
 import { Signup } from '../signup/signup';
 import { NgFor } from '@angular/common';
 
@@ -14,11 +14,22 @@ export class Login {
   x=signal(10);
   y=signal(20);
   z=computed(()=>this.x()*this.y());
+  m=signal(0);
  
   increment(){
-    this.x.set(this.x()+10);
+    this.x.set(this.x()+1);
     console.log(this.z());
 
+  }
+
+  constructor(){
+   effect(()=>{
+    if(this.x()%4===0){
+      this.m.set(0);
+    }else{
+      this.m.set(1);
+    }
+   });
   }
 
 
